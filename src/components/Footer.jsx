d5f49a26d7fdfe3b67ee2269ad076eadf965235b
vkassingh <44vikasrajput44@gmail.com>
2026-01-31 12:09:16 +0530

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -61,7 +61,7 @@ const Footer = () => {
             <ul className="space-y-2">
               <li>
                 <Link
-                  to="/saroj-institute-artificial-intelligence"
+                  to="/academics/Saroj-institute-of-artificial-intelligence"
                   className="text-gray-200 hover:text-yellow-400 hover:underline hover:underline-offset-8 transition-colors duration-300"
                 >
                   Saroj Institute of Artificial Intelligence
